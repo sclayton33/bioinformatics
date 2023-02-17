4fdec7fadc91e7c523b17b52b29cf00ef2f75bf2
user <stu3cla@protonmail.com>
2023-02-17 02:09:54 -0500

    ballgown de R file

---
+++ RNA-seq/BP_PRJNA496042/Ballgown_DE.R
@@ -0,0 +1,61 @@
+# load the required libraries
+library(ballgown)
+library(genefilter)
+library(dplyr)
+library(devtools)
+
+# Create phenotype data needed for ballgown analysis
+ids=c("UHR_Rep1","UHR_Rep2","UHR_Rep3","HBR_Rep1","HBR_Rep2","HBR_Rep3")
+type=c("UHR","UHR","UHR","HBR","HBR","HBR")
+results="/home/user/bioinformatics/ref_genome/test_data/align_output/expression/stringtie/ref_only/"
+path=paste(results,ids,sep="")
+pheno_data=data.frame(ids,type,path)
+
+# Load ballgown data structure and save it to a variable "bg"
+bg = ballgown(samples=as.vector(pheno_data$path), pData=pheno_data)
+
+# Display a description of this object
+bg
+
+# Load all attributes including gene name
+bg_table = texpr(bg, 'all')
+bg_gene_names = unique(bg_table[, 9:10])
+
+# Save the ballgown object to a file for later use
+save(bg, file='bg.rda')
+
+# Perform differential expression (DE) analysis with no filtering
+results_transcripts = stattest(bg, feature="transcript", covariate="type", getFC=TRUE, meas="FPKM")
+results_genes = stattest(bg, feature="gene", covariate="type", getFC=TRUE, meas="FPKM")
+results_genes = merge(results_genes, bg_gene_names, by.x=c("id"), by.y=c("gene_id"))
+
+# Save a tab delimited file for both the transcript and gene results
+write.table(results_transcripts, "UHR_vs_HBR_transcript_results.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+write.table(results_genes, "UHR_vs_HBR_gene_results.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+
+# Filter low-abundance genes. Here we remove all transcripts with a variance across the samples of less than one
+bg_filt = subset (bg,"rowVars(texpr(bg)) > 1", genomesubset=TRUE)
+
+# Load all attributes including gene name
+bg_filt_table = texpr(bg_filt , 'all')
+bg_filt_gene_names = unique(bg_filt_table[, 9:10])
+
+# Perform DE analysis now using the filtered data
+results_transcripts = stattest(bg_filt, feature="transcript", covariate="type", getFC=TRUE, meas="FPKM")
+results_genes = stattest(bg_filt, feature="gene", covariate="type", getFC=TRUE, meas="FPKM")
+results_genes = merge(results_genes, bg_filt_gene_names, by.x=c("id"), by.y=c("gene_id"))
+
+# Output the filtered list of genes and transcripts and save to tab delimited files
+write.table(results_transcripts, "UHR_vs_HBR_transcript_results_filtered.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+write.table(results_genes, "UHR_vs_HBR_gene_results_filtered.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+
+# Identify the significant genes with p-value < 0.05
+sig_transcripts = subset(results_transcripts, results_transcripts$pval<0.05)
+sig_genes = subset(results_genes, results_genes$pval<0.05)
+
+# Output the significant gene results to a pair of tab delimited files
+write.table(sig_transcripts, "UHR_vs_HBR_transcript_results_sig.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+write.table(sig_genes, "UHR_vs_HBR_gene_results_sig.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+
+# Exit the R session
+quit(save="no")
